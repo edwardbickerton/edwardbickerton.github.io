ac7f37be46c29069fb1734978448fb5368c03284
Edward <edwardbickerton@icloud.com>
2024-08-18 19:36:03 +0100

    Overhaul

--- javascript/website.js
+++ javascript/website.js
@@ -1,12 +1,12 @@
 // Reuseable header (buy btc button <a href="/html/blog_posts/bisq.html"><button class="header_btn" id="buy_btn">buy BTC</button></a>)
-const headerTemplate = document.createElement('template');
+const headerTemplate = document.createElement("template");
 headerTemplate.innerHTML = `
   <link rel="stylesheet" href="/CSS/style.css" />
   <header>
     <nav>
-      <a href="/index.html"><button class="header_btn">home</button></a>
-      <a href="/html/blog.html"><button class="header_btn">blog</button></a>
-      <a href="/html/node.html"><button class="header_btn">node</button></a>
+      <a href="/index.html"><button class="header_btn">Home</button></a>
+      <a href="https://github.com/edwardbickerton" target="_blank"><button class="header_btn">GitHub</button></a>
+      <a href="https://www.linkedin.com/in/edward-bickerton/" target="_blank"><button class="header_btn">LinkedIn</button></a>
     </nav>
     <hr>
   </header>
@@ -18,36 +18,30 @@ class Header extends HTMLElement {
   }
 
   connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    const shadowRoot = this.attachShadow({ mode: "closed" });
     shadowRoot.appendChild(headerTemplate.content);
   }
 }
 
-customElements.define('header-component', Header);
+customElements.define("header-component", Header);
 // End of reuseable header
 
 // Reuseable footer
-const footerTemplate = document.createElement('template');
+const footerTemplate = document.createElement("template");
 footerTemplate.innerHTML = `
   <link rel="stylesheet" href="/CSS/style.css" />
   <br>
   <footer>
     <hr>
     <ul id="footer_list">
-      <li><a href="mailto:satsuma_ln@icloud.com"><button class="footer_btn">
-        Email
+      <li><a href="/index.html"><button class="footer_btn">
+        Home
       </button></a></li>
-      <!--
-      <li><a href="https://twitter.com/Satsuma_LN" target=_blank><button class="footer_btn">
-        Twitter
-      </button></a></li>
-      -->
-      <li><a href="https://t.me/Satsuma_LN" target=_blank><button class="footer_btn">
-        Telegram
-      </button></a></li>
-
-      <li><a href="https://github.com/Satsuma-LN" target=_blank><button class="footer_btn">
+      <li><a href="https://github.com/edwardbickerton" target="_blank"><button class="footer_btn">
         GitHub
+      </button></a></li>
+      <li><a href="https://www.linkedin.com/in/edward-bickerton/" target=_blank><button class="footer_btn">
+        LinkedIn
       </button></a></li>
 
     </ul>
@@ -60,10 +54,10 @@ class Footer extends HTMLElement {
   }
 
   connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    const shadowRoot = this.attachShadow({ mode: "closed" });
     shadowRoot.appendChild(footerTemplate.content);
   }
 }
 
-customElements.define('footer-component', Footer);
+customElements.define("footer-component", Footer);
 // End of reuseable footer
